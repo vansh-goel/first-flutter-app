59750c418608cc27ecf91848d44b112875aefc80
goelvansh2 <goelvansh2@gmail.com>
2024-01-25 14:15:22 +0000

    Manuevering to Make this a music app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/themes/themes_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,42 +23,8 @@ class MyApp extends StatelessWidget {
     log(1);
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("~ hello ~"),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange,
-          elevation: 0,
-          leading: Icon(Icons.menu_rounded),
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.logout_rounded))
-          ],
-        ),
-        backgroundColor: Colors.amber[200],
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.deepOrange,
-              borderRadius: BorderRadius.circular(20),
-            ),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Text(
-                  "hello World",
-                  style: TextStyle(color: Colors.white, fontSize: 30, fontFamily: 'monospace', fontWeight: FontWeight.bold),
-                ),
-                Icon(
-                  Icons.rocket_launch_outlined, color: Colors.black54, size: 50,
-                )
-                ]
-              )
-            )
-          ),
-        ),
+      home: HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
